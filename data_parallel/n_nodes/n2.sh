d65034df71ca3be2dcefe0879bebbe54868e035a
buddhi1 <buddhiashan8@gmail.com>
2025-03-17 02:06:02 -0500

    data parallel handson added

---
+++ data_parallel/n_nodes/n2.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Set TF_CONFIG for the Chief Worker on Node 1
+export TF_CONFIG='{
+  "cluster": {
+    "worker": ["gpu027:2222", "gpu028:2222"]
+  },
+  "task": {"type": "worker", "index": 1}
+}'
+# export TF_CONFIG='{
+#   "cluster": {
+#     "worker": ["gpu028:2222"]
+#   },
+#   "task": {"type": "worker", "index": 0}
+# }'
+# export TF_CONFIG='{
+#   "cluster": {
+#     "worker": ["localhost:2222", "localhost:2422"]
+#   },
+#   "task": {"type": "worker", "index": 1}
+# }'
+
+# Run training script
+python main.py
