4c64274a60a8af2ea2d3cde7326e786c7352652f
buddhi1 <bxashan@gmail.com>
2026-02-10 23:52:17 -0600

    folder names updated

--- HP-ML/CUDA/parallel-programming/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#SBATCH -J hello_world_cudac
-#SBATCH -o hello_world_out.txt
-#SBATCH -p gpu1v100
-#SBATCH -t 00:05:00
-#SBATCH -N 1
-#SBATCH -n 1
-ml cuda/toolkit/11.2
-./helloworld-2 #the executable obtained after compiling the program
